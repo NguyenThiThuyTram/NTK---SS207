60ebc81b67bd2458d77ef35b3dbd4988a6f8c81e
tpkhai108 <tpkhai108@gmail.com>
2026-05-24 19:19:24 +0700

    Update forgot_password.php

--- src/views/forgot_password.php
+++ src/views/forgot_password.php
@@ -93,7 +93,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 }
             }
         } catch (PDOException $e) {
-            die("Lỗi Database: " . $e->getMessage());
+            // Handle error silently
         }
     }
 }
